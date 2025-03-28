b1e44c619e611260acf06b4362a8cf513d669080
saranzafar <saran.development@gmail.com>
2025-03-28 10:00:00 +0500

    almost complete

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,14 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+*
+* @type {import('@react-native/metro-config').MetroConfig}
+*/
 const config = {};
 
+module.exports = wrapWithReanimatedMetroConfig(config);
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
